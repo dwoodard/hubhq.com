4635d2378b45dc2eeb1591874f075763842e3539
Dustin Woodard <dustin.woodard@gmail.com>
2021-04-23 19:40:45 -0600

    add app to repo

---
+++ resources/app/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    // '@vue/airbnb',
+  ],
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off'
+  },
+};
---
+++ resources/app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav">
+    change
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #b00000;
+}
+</style>
